662453d7771fe40a299b0a6c428a94becc8187e0
Eddian <71007236+EdisekCZ@users.noreply.github.com>
2022-11-14 22:04:19 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
 from kivy.app import App
-from kivy.uix.relativelayout import RelativeLayout
+from matplotlib import pyplot as plt
+from kivy.lang import Builder
+import numpy as np
+from kivy.garden.matplotlib import FigureCanvasKivyAgg
 
 
-class MyRelativeLayout (RelativeLayout):
-    pass
+class GraphApp(App):
 
-
-class KreslimCaru(App):
     def build(self):
+        self.str = Builder.load_string(""" 
 
-        return MyRelativeLayout()
+BoxLayout:
+    layout:layout
+
+    BoxLayout:
+
+        id:layout
+
+                                """)
+
+        y = [3, 5, 1, 5 , 8 ,4]
+
+        y = np.array(y)
+
+        plt.plot(y)
+
+        plt.xlabel('X')
+
+        plt.ylabel('Y')
+        plt.grid(True, color='lightgrey')
+
+        self.str.layout.add_widget(FigureCanvasKivyAgg(plt.gcf()))
+        return self.str
 
 
-if __name__ == "__main__":
-    window = KreslimCaru()
-    window.run()
+GraphApp().run()
